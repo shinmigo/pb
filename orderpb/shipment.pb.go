a717f52f5e4ac2e0a1c6499635252ad8bea6a6ef
shrimp <1732341@qq.com>
2020-10-19 10:37:24 +0800

    payment移到service-shop

--- orderpb/shipment.pb.go
+++ orderpb/shipment.pb.go
@@ -9,8 +9,6 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	basepb "github.com/shinmigo/pb/basepb"
 	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -157,14 +155,6 @@ func (c *shipmentServiceClient) AddShipment(ctx context.Context, in *Shipment, o
 // ShipmentServiceServer is the server API for ShipmentService service.
 type ShipmentServiceServer interface {
 	AddShipment(context.Context, *Shipment) (*basepb.AnyRes, error)
-}
-
-// UnimplementedShipmentServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedShipmentServiceServer struct {
-}
-
-func (*UnimplementedShipmentServiceServer) AddShipment(ctx context.Context, req *Shipment) (*basepb.AnyRes, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AddShipment not implemented")
 }
 
 func RegisterShipmentServiceServer(s *grpc.Server, srv ShipmentServiceServer) {
